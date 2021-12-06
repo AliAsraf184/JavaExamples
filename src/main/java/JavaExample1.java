9e790b79171ac314ec66b7c669bdc5abb509e52b
Ali Ashrafli <ali.asraf@list.ru>
2021-12-06 21:45:29 +0400

    Initial commit

---
+++ src/main/java/JavaExample1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class JavaExample1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int [] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            
+        }
+    }
+}
